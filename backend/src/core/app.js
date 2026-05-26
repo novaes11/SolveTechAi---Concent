d0cd44148995d631fffdcdd1ae1cb41eb1c88c3e
João Vitor Novaes <jvn.novaes@edu.unifil.br>
2026-05-26 20:13:53 -0300

    fix: ajustado bug de require do arquivo ./logger.js

--- backend/src/core/app.js
+++ backend/src/core/app.js
@@ -1,10 +1,8 @@
-let log = new Logger('app');
+const Logger = require('./logger');
+const log = new Logger('app');
 
 // Load environment variables from .env file, this allows us to keep sensitive information like database credentials out of our source code and easily manage different configurations for development, testing, and production environments.
 require('dotenv').config();
-
-// Importing the Logger class from the Logger.js file, this class is used to handle logging with different levels and colors, making it easier to track the application's behavior and debug issues.
-const expess = require('express');
 
 log.info('Starting application...');
 
@@ -13,7 +11,7 @@ const db = require('../config/database');
 async function testDBConnection(){
     const createPersonTableQuery = `
         CREATE TABLE IF NOT EXISTS person (
-            id_person AUTOINCREMENT PRIMARY KEY,
+            id_person SERIAL PRIMARY KEY,
             name VARCHAR(255) NOT NULL,
             biological_sex CHAR NOT NULL,
             date_of_birth DATE NOT NULL,
