b7b994bbc84cc2c9f31e2f030ce11b8e8a7e6e31
João Vitor Novaes <jvn.novaes@edu.unifil.br>
2026-05-27 19:11:14 -0300

    fix: adicionado função para criar conexão ao banco de dados e teste de conexão

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,19 +2,54 @@ const Logger  = require('../core/logger')
 const log = new Logger('backend/src/config/database.js');
 // this is called Destructuring assignment in JavaScript, it allows us to extract properties from an object and assign them to variables. In this case, we are extracting the Pool class from the 'pg' library and assigning it to a variable named Pool.
 const { Pool } = require('pg');
-try{
-    const dbPool = new Pool({
-        user: process.env.DB_USER,
-        host: process.env.DB_HOST,
-        database: process.env.DB_NAME,
-        password: process.env.DB_PASS,
-        port: process.env.DB_PORT,      
-    })
 
-    // Singleton method to ensure that only one instance of the database connection pool is created and shared across the application. This helps to manage resources efficiently and avoid unnecessary connections to the database. By exporting the dbPool instance, other parts of the application can import and use it to interact with the database without needing to create multiple instances of the connection pool.
-    module.exports = dbPool;
-    
-    log.info('Database connection pool created successfuly ✅');
-}catch(error){
-    log.error(error)
+async function createDBPool(retries = 5, delay = 2000) {
+    log.info('Creating database connection pool...');
+    for (let i = 0; i < retries; i++) {
+        try{
+            const dbPool = new Pool({
+                user: process.env.DB_USER,
+                host: process.env.DB_HOST,
+                database: process.env.DB_NAME,
+                password: process.env.DB_PASS,
+                port: process.env.DB_PORT,      
+            })
+
+            // Singleton method to ensure that only one instance of the database connection pool is created and shared across the application. This helps to manage resources efficiently and avoid unnecessary connections to the database. By exporting the dbPool instance, other parts of the application can import and use it to interact with the database without needing to create multiple instances of the connection pool.
+            module.exports = dbPool;
+
+            log.info('Database connection pool created successfuly ✅');
+            return true;    
+        }catch(error){
+            if (i < retries - 1) {
+                log.warning(`Database connection pool creation failed (Attempt ${i + 1}/${retries}). Try again in ${delay} ms. Error: ${error.message}`);
+                await new Promise(res => setTimeout(res, delay));
+            } else {
+                log.error(`Database connection pool creation failed after ${retries} attempts. Error: ${error.message}`);
+                throw error;
+            }
+        }   
+    }
 }
+async function testDBConnection(retries = 5, delay = 2000) {
+    log.info('Testing database connection...');
+    
+    for (let i = 0; i < retries; i++) {
+        try{
+            await db.query(createPersonTableQuery); 
+            log.info('Database connection pool working correctly ✅');
+            return true;
+
+        } catch(error){
+            if (i < retries - 1) {
+                log.warning(`Database connection failed (Attempt ${i + 1}/${retries}). Try again in ${delay} ms. Error: ${error.message}`);
+                await new Promise(res => setTimeout(res, delay));
+            } else {
+                log.error(`Database connection failed after ${retries} attempts. Error: ${error.message}`);
+                return false;
+            }
+        }
+    }
+}  
+createDBPool();
+testDBConnection();
