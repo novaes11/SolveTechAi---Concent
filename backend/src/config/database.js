e27c7e68c8eeacd0a0984404dbbe92284aee5cab
João Vitor Novaes <jvn.novaes@edu.unifil.br>
2026-05-26 23:37:52 -0300

    fix: ajustado erro de bsuca de dados .env

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,8 +1,7 @@
 const Logger  = require('../core/logger')
-const log = new Logger('database');
+const log = new Logger('backend/src/config/database.js');
 // this is called Destructuring assignment in JavaScript, it allows us to extract properties from an object and assign them to variables. In this case, we are extracting the Pool class from the 'pg' library and assigning it to a variable named Pool.
 const { Pool } = require('pg');
-
 try{
     const dbPool = new Pool({
         user: process.env.DB_USER,
@@ -15,8 +14,7 @@ try{
     // Singleton method to ensure that only one instance of the database connection pool is created and shared across the application. This helps to manage resources efficiently and avoid unnecessary connections to the database. By exporting the dbPool instance, other parts of the application can import and use it to interact with the database without needing to create multiple instances of the connection pool.
     module.exports = dbPool;
     
-    log.info('Database connection pool created successfully ✅');
+    log.info('Database connection pool created successfuly ✅');
 }catch(error){
     log.error(error)
 }
-
