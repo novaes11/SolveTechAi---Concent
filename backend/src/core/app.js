5abfe679adb4dc1e1ba31b452057145f81506e85
João Vitor Novaes <jvn.novaes@edu.unifil.br>
2026-06-02 20:18:44 -0300

    fix: removido function para criar tabel Person

--- backend/src/core/app.js
+++ backend/src/core/app.js
@@ -7,35 +7,3 @@ require('dotenv').config({ path: '.env' });
 log.info('Starting application...');
 
 const db = require('../config/database');
-
-const createPersonTableQuery = `
-        CREATE TABLE IF NOT EXISTS person (
-            id_person SERIAL PRIMARY KEY,
-            name VARCHAR(255) NOT NULL,
-            biological_sex CHAR NOT NULL,
-            date_of_birth DATE NOT NULL,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        )
-    `
-testDBConnection();
-
-async function testDBConnection(retries = 5, delay = 2000) {
-    log.info('Testing database connection...');
-    
-    for (let i = 0; i < retries; i++) {
-        try{
-            await db.query(createPersonTableQuery); 
-            log.info('Database connection pool working correctly ✅');
-            return true;
-
-        } catch(error){
-            if (i < retries - 1) {
-                log.warning(`Database connection failed (Attempt ${i + 1}/${retries}). Try again in ${delay} ms. Error: ${error.message}`);
-                await new Promise(res => setTimeout(res, delay));
-            } else {
-                log.error(`Database connection failed after ${retries} attempts. Error: ${error.message}`);
-                return false;
-            }
-        }
-    }
-}   
