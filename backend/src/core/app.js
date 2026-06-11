828195be216c0672ebedce4f8862f07b55818e2a
João Vitor Novaes <jvn.novaes@edu.unifil.br>
2026-06-10 23:58:13 -0300

    fix: ajustado estrutura e orgazinado codigos

--- backend/src/core/app.js
+++ backend/src/core/app.js
@@ -1,9 +1,19 @@
-const Logger = require('./logger');
-const log = new Logger('backend/src/core/app.js');
-
 // Load environment variables from .env file, this allows us to keep sensitive information like database credentials out of our source code and easily manage different configurations for development, testing, and production environments.
 require('dotenv').config({ path: '.env' });
 
+const Logger = require('./logger');
+const log = new Logger('backend/src/core/app.js');
+
 log.info('Starting application...');
 
-const db = require('../config/database');
+const { dbPool, testDBConnection} = require('../config/database');
+
+async function bootstrap(){
+    log.info('Starting aplication...')
+
+    await testDBConnection();
+
+    log.info('Application started successfully. Moving to next steps...');
+}
+
+bootstrap();
