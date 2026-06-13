f2bcae4209dbcb345d5e933d271eeb1311b6eafe
João Vitor Novaes <jvn.novaes@edu.unifil.br>
2026-06-13 13:09:27 -0300

    featuer: criado arquivo .js para personModel

---
+++ backend/src/models/personModel.js
@@ -0,0 +1,29 @@
+// let { dbPool } = require('../config/database');
+let Logger = require('../core/logger');
+let log = new Logger('backend/src/models/personModel.js');
+
+const personModel = {
+    createPerson: async (name, biological_sex, date_of_birth) => {
+        log.info('Creating person...');
+        log.debug(`Received paramters: name=${name}, biological_sex=${biological_sex}, date_of_birth=${date_of_birth}`);
+        const query = `
+            INSERT INTO person (
+                name, biological_sex, date_of_birth
+            ) VALUES ($1, $2, $3)
+             RETURNING *
+             `;
+        
+        const values = [name, biological_sex, date_of_birth];
+
+        try {
+            log.debug(`Executing query: ${query} with values: ${values}`);
+            const res = await dbPool.query(query, values);
+            log.info('Person created successfully!');
+            return res.rows[0];
+        } catch (error) {
+            log.error(`Error creating person: ${error.message}`); 
+            throw error;
+        }
+    }
+}
+personModel.createPerson('John Doe', 'M', '1990-01-01');
