194483960c4524762c3b77fcf80298e4eb3a23b6
João Vitor Novaes <jvn.novaes@edu.unifil.br>
2026-07-09 22:03:19 -0300

    feat: Update person model query to include CPF

--- backend/src/models/personModel.js
+++ backend/src/models/personModel.js
@@ -1,27 +1,30 @@
-// let { dbPool } = require('../config/database');
-let Logger = require('../core/logger');
-let log = new Logger('backend/src/models/personModel.js');
+const { dbPool } = require('../config/database');
+const Logger = require('../core/logger');
+const log = new Logger('backend/src/models/personModel.js');
 
 const personModel = {
-    createPerson: async (name, biological_sex, date_of_birth) => {
+    createPerson: async (name, cpf, biological_sex, birth_date) => {
         log.info('Creating person...');
-        log.debug(`Received paramters: name=${name}, biological_sex=${biological_sex}, date_of_birth=${date_of_birth}`);
+        log.debug(`Received paramters: name=${name}, cpf=${cpf}, biological_sex=${biological_sex}, birth_date=${birth_date}`);
         const query = `
             INSERT INTO person (
-                name, biological_sex, date_of_birth
-            ) VALUES ($1, $2, $3)
-             RETURNING *
+                name, cpf, biological_sex, birth_date
+            ) VALUES ($1, $2, $3, $4) ON CONFLICT (cpf) DO NOTHING
+             RETURNING id_person
              `;
-        
-        const values = [name, biological_sex, date_of_birth];
+
+        const values = [name, cpf, biological_sex, birth_date];
 
         try {
             log.debug(`Executing query: ${query} with values: ${values}`);
             const res = await dbPool.query(query, values);
+            if (res.rows.length === 0) {
+                throw log.error(`Person with cpf ${cpf} already exists`, 'createPerson');
+            }
             log.info('Person created successfully!');
             return res.rows[0];
         } catch (error) {
-            log.error(`Error creating person: ${error.message}`); 
+            log.error(`Error creating person: ${error.message}`);
             throw error;
         }
     }
