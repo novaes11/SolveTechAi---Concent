cdf466cb5fad4f11247d4602a14e89fe343af0ef
João Vitor Novaes <jvn.novaes@edu.unifil.br>
2026-07-09 22:03:19 -0300

    chore: Organize appointment routes structure

---
+++ backend/src/routes/appointmentRoute.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const appointmentController = require('../controllers/appointmentController');
+
+router.post('/', appointmentController.createAppointment);
+
+// router.get('/', appointmentController.getAllAppointments);
+// router.get('/:id', appointmentController.getAppointmentById);
+// router.put('/:id', appointmentController.updateAppointment);
+// router.delete('/:id', appointmentController.deleteAppointment);
+
+module.exports = router;
